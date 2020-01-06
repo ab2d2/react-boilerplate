a1f0000debaa3260fa00dde0965843a9bd8cebbf
Abdul Rahimi <abdul.rahimi_defra@outlook.com>
2020-01-06 22:54:18 +0000

    added react-axe for a11y

--- src/index.js
+++ src/index.js
@@ -6,6 +6,11 @@ import DefaultErrorBoundary from './DefaultErrorBoundary'
 
 import '@babel/polyfill'
 
+if (process.env.NODE_ENV === 'development') {
+  const axe = require('react-axe')
+  axe(React, ReactDOM, 1000)
+}
+
 ReactDOM.render(
   <React.StrictMode>
     <DefaultErrorBoundary>
